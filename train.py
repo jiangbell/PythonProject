32e80c45e9d1fdfa055bbc26f407bd5a6ceff989
jiangjc <90j_jinc@sina.com>
2022-07-18 21:39:01 +0800

    模型训练代码提交

---
+++ train.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from model import LeNet
+from data import data_train_loader as data_train_loader
+
+model = LeNet()
+model.train()
+lr = 0.01
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+
+train_loss = 0
+correct = 0
+total = 0
+
+for batch_idx, (inputs, targets) in enumerate(data_train_loader):
+    optimizer.zero_grad()
+    outputs = model(inputs)
+    loss = criterion(outputs, targets)
+    loss.backward()
+    optimizer.step()
+
+    train_loss += loss.item()
+    _, predicted = outputs.max(1)
+    total += targets.size(0)
+    correct += predicted.eq(targets).sum().item()
+
+    print(batch_idx, len(data_train_loader),
+          'Loss:%.3f | Acc:%.3f%%(%d/%d)' % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
