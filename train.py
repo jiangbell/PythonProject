a6503c48d5c92a3e30b14573e0c88c20c5aaae35
jiangjc <90j_jinc@sina.com>
2022-07-19 21:10:21 +0800

    train.py 新增模型保存代码 inference.py 新增模型预测代码

--- train.py
+++ train.py
@@ -27,3 +27,13 @@ for batch_idx, (inputs, targets) in enumerate(data_train_loader):
 
     print(batch_idx, len(data_train_loader),
           'Loss:%.3f | Acc:%.3f%%(%d/%d)' % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+save_info = {
+    "iter_num": batch_idx,
+    "optimizer": optimizer.state_dict(),
+    "model": model.state_dict(),
+}
+
+save_path = "./model.pth"
+
+torch.save(save_info, save_path)
